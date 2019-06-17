54e5855cf1bdade700403f2ec952a6f3d4cada33
Ravi Inder Manshahia <ravinder@lab91.co>
2019-06-17 19:53:32 +0530

    Initial Commit

---
+++ Property-job-test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Property-job-test
+//
+//  Created by Ravi Inder Manshahia on 17/06/19.
+//  Copyright © 2019 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
